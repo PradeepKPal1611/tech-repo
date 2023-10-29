a721ba5c50789773e9cfeebd3a543948da60e75c
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-10-29 17:59:25 +0530

    Modified Src|App/assets/component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/header';
+import Hero from './components/hero';
 
 function App() {
   return (
     <div className="App">
       <h1>I am app.js</h1>
       < Header/>
+      < Hero />
     </div>
   );
 }
---
+++ src/components/hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import '../assets/css/Hero.css';
+
+function Hero() {
+    return(
+        <div className="hero-container">
+            <h1>Welcome to Our Website</h1>
+            <p>Discover amazing content and services.</p>
+            <button>Get Started</button>
+        </div>
+    );
+}
+export default Hero;
