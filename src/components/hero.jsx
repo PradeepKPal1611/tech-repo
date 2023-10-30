9376529e42d7397a24f65ef999a967db7fd7a802
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-10-30 14:45:25 +0530

    Images|Modified/Package/hero.js

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
+import img from '../assets/images/Image3.jpg';
+
 import '../assets/css/Hero.css';
 
 function Hero() {
     return(
         <div className="hero-container">
+            <img alt="HeroImg" src={img} />
             <h1>Welcome to Our Website</h1>
             <p>Discover amazing content and services.</p>
             <button>Get Started</button>
